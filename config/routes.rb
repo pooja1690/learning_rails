1d5833b8ae77bf33d270108f7749bc1fb68264b7
Ashok <ashok@Poojas-MBP-2.attlocal.net>
2016-03-27 12:00:51 -0500

    made users as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,8 +7,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
-  
+  root 'users#index'  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
