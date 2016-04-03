2bdae88b7f009aeeeeeddf7c7b9bee77b3555a7b
pooja shyam <pooja.1690@gmail.com>
2016-04-03 13:25:52 -0500

    add email check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,15 +25,14 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+        if User.exists?(:email => @user.email)
+          format.html { redirect_to @user, notice: 'User exists.' }
+        else
+          @user.save
+          format.html { redirect_to @user, notice: 'User was successfully created.' }
+          format.json { render :show, status: :created, location: @user }
+        end
     end
   end
 
@@ -69,6 +68,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password)
     end
 end
